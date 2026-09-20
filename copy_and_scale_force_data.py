02b8d66cabeb8cc4f29f41a473940637919dc7ea
Mihir Pullabhotla <mihirpulla1928@gmail.com>
2026-09-19 18:42:16 -0700

    Fix variable names in copy_and_scale_force_data

--- copy_and_scale_force_data.py
+++ copy_and_scale_force_data.py
@@ -15,9 +15,6 @@ class VectorOverlay:
     fps_annotated = 120
     " abt fps"
     force_scaling = 0.8
-
-    frame_width = int(cap.get(cap.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cap.CAP_PROP_FRAME_HEIGHT))
     
     def copy_and_scale_force_data(self, df_aligned):
         """
@@ -30,33 +27,36 @@ class VectorOverlay:
         Returns:
         Scaled Dataframe with scaled force data.
         """
+
+        frame_width = int(cap.get(cap.CAP_PROP_FRFME_WIDTH))
+        frame_height = int(cap.get(cap.CAP_PROP_FRFME_HEIGHT))
         # Copy the force data from the DataFrame
         force_data = df_aligned.copy()
 
         # Calculate maximum absolute values out of all forces
-        max_z_force = max(force_data['FP1_Az'].abs().max(), df_aligned['FP2_Az'].abs().max())
-        max_x_force = max(force_data['FP1_Ax'].abs().max(), df_aligned['FP2_Ax'].abs().max())
-        max_y_force = max(force_data['FP1_Ay'].abs().max(), df_aligned['FP2_Ay'].abs().max())
+        max_z_force = max(force_data['FP1_Fz'].abs().max(), df_aligned['FP2_Fz'].abs().max())
+        max_x_force = max(force_data['FP1_Fx'].abs().max(), df_aligned['FP2_Fx'].abs().max())
+        max_y_force = max(force_data['FP1_Fy'].abs().max(), df_aligned['FP2_Fy'].abs().max())
         max_total_force = max(max_x_force, max_y_force, max_z_force)
 
         # Sets each force to a value between 0 and 1(1 being the maximum force) in order to apply scale factor
-        force_data['FP1_Az'] = (force_data['FP1_Az']/max_total_force).round(4)
-        force_data['FP1_Ay'] = (force_data['FP1_Ay']/max_total_force).round(4)
-        force_data['FP2_Az'] = (force_data['FP2_Az']/max_total_force).round(4)
-        force_data['FP2_Ay'] = (force_data['FP2_Ay']/max_total_force).round(4)
-        force_data['FP1_Ax'] = (force_data['FP1_Ax']/max_total_force).round(4)
-        force_data['FP2_Ax'] = (force_data['FP2_Ax']/max_total_force).round(4)
+        force_data['FP1_Fz'] = (force_data['FP1_Fz']/max_total_force).round(4)
+        force_data['FP1_Fy'] = (force_data['FP1_Fy']/max_total_force).round(4)
+        force_data['FP2_Fz'] = (force_data['FP2_Fz']/max_total_force).round(4)
+        force_data['FP2_Fy'] = (force_data['FP2_Fy']/max_total_force).round(4)
+        force_data['FP1_Fx'] = (force_data['FP1_Fx']/max_total_force).round(4)
+        force_data['FP2_Fx'] = (force_data['FP2_Fx']/max_total_force).round(4)
 
         # Determines and applies scale factor to force 0-1 values
         # Max height of a force is 0.8 frame height
         scale_factor = self.frame_height*self.force_scaling
 
-        force_data['FP1_Az'] *= scale_factor.astype(int)
-        force_data['FP1_Ay'] *= scale_factor.astype(int)
-        force_data['FP2_Az'] *= scale_factor.astype(int)
-        force_data['FP2_Ay'] *= scale_factor.astype(int)
-        force_data['FP1_Ax'] *= scale_factor.astype(int)
-        force_data['FP2_Ax'] *= scale_factor.astype(int)
+        force_data['FP1_Fz'] *= scale_factor.astype(int)
+        force_data['FP1_Fy'] *= scale_factor.astype(int)
+        force_data['FP2_Fz'] *= scale_factor.astype(int)
+        force_data['FP2_Fy'] *= scale_factor.astype(int)
+        force_data['FP1_Fx'] *= scale_factor.astype(int)
+        force_data['FP2_Fx'] *= scale_factor.astype(int)
 
         return force_data
 
